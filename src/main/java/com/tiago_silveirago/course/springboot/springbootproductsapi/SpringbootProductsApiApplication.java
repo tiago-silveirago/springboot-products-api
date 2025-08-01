475828e51ccf54bb061fe5eebc4d64b4326fbc45
Tiago Silveira <tiago.silveirago@gmail.com>
2025-08-01 16:16:10 -0300

    chore: initial commit

---
+++ src/main/java/com/tiago_silveirago/course/springboot/springbootproductsapi/SpringbootProductsApiApplication.java
@@ -0,0 +1,13 @@
+package com.tiago_silveirago.course.springboot.springbootproductsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProductsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootProductsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tiago_silveirago/course/springboot/springbootproductsapi/SpringbootProductsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiago_silveirago.course.springboot.springbootproductsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootProductsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
